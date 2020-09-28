9d5d27e8fc152ce60787147b3a9e2f8aad978fa2
Abhishek <abhisheksunkuru@gmail.com>
2020-09-28 15:32:15 +0530

    Added rails internationlization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale_params || I18n.default_locale
+  end
+
+  def extract_locale_params
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
